8380cec550dabc6414ed32d0b750033db24a553d
Aditya <vaditya06@gmail.com>
2024-02-07 01:49:00 -0500

    Manual implementation of ARP protocol

--- Network_Scanner/network_scanner.py
+++ Network_Scanner/network_scanner.py
@@ -3,8 +3,17 @@
 import scapy.all as scapy
 
 
+BROADCAST_MAC = "FF:FF:FF:FF:FF:FF"
+
 def scan(ip):
     scapy.arping(ip)
 
 
-scan("192.168.146.1/24")
+def scan_manual(ip):
+    arp_request = scapy.ARP(pdst=ip, hwdst=BROADCAST_MAC)
+    # arp_request.pdst = ip
+    print(arp_request.summary())
+    # scapy.ls(scapy.ARP()) # Lists all the fields which can be set
+
+
+scan_manual("192.146.168.1/24")
