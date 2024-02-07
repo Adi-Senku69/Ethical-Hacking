902e7a507f662c36be69d7f54ac104252a3e061b
Aditya <vaditya06@gmail.com>
2024-02-07 01:58:22 -0500

    Added new packet frames

--- Network_Scanner/network_scanner.py
+++ Network_Scanner/network_scanner.py
@@ -3,17 +3,18 @@
 import scapy.all as scapy
 
 
-BROADCAST_MAC = "FF:FF:FF:FF:FF:FF"
+BROADCAST_MAC = "ff:ff:ff:ff:ff:ff"
 
 def scan(ip):
     scapy.arping(ip)
 
 
 def scan_manual(ip):
-    arp_request = scapy.ARP(pdst=ip, hwdst=BROADCAST_MAC)
+    arp_request = scapy.ARP(pdst=ip)
     # arp_request.pdst = ip
-    print(arp_request.summary())
     # scapy.ls(scapy.ARP()) # Lists all the fields which can be set
-
+    broadcast = scapy.Ether(dst=BROADCAST_MAC)
+    arp_request_broadcast = broadcast/arp_request
+    print(arp_request_broadcast.show())
 
 scan_manual("192.146.168.1/24")
