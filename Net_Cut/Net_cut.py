97d324a7de73a54fa9d7a3e60755a480f0dc9bbf
Aditya <vaditya06@gmail.com>
2024-03-12 03:54:43 -0400

    Net Cut program completed

---
+++ Net_Cut/Net_cut.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# iptables -I FORWARD -j NFQUEUE --queue-num [queue num]
+import netfilterqueue as net
+
+def process_packet(packet):
+    print(packet)
+    packet.drop() # All the packets will be dropped
+
+queue = net.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
